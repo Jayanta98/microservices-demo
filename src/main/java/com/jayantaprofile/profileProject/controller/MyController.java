63dbc2663700522aaeb9c4b7c708fb1a55e9d06f
Jayanta <jg.16u10555@btech.nitdgp.ac.in>
2021-05-30 19:39:53 +0530

    ok

---
+++ src/main/java/com/jayantaprofile/profileProject/controller/MyController.java
@@ -0,0 +1,39 @@
+package com.jayantaprofile.profileProject.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class MyController {
+	
+	@Value("${spring.message}")
+	private String msg;
+	
+	@Value("${spring.author}")
+	private String author;
+	
+	@Value("${student.names}")
+	private List<String> names;
+	
+	@Value("#{${users}}")
+	private HashMap<String,String> users;
+	
+	@GetMapping("hello")
+	public String welcome() {
+		System.out.println(names.get(1));
+		System.out.println(users.get("test1"));
+		return msg;
+	}
+	@GetMapping("author")
+	public String author() {
+		return author;
+	}
+
+}
