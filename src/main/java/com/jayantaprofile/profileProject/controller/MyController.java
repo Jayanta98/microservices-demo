d0620482002ba2e3f673e432f45cfdc92754f9a8
Jayanta <jg.16u10555@btech.nitdgp.ac.in>
2021-05-30 20:00:59 +0530

    final changes from local

--- src/main/java/com/jayantaprofile/profileProject/controller/MyController.java
+++ src/main/java/com/jayantaprofile/profileProject/controller/MyController.java
@@ -33,7 +33,9 @@ public class MyController {
 	}
 	@GetMapping("author")
 	public String author() {
-		return author+"he is the best author of India .This is from the github. Full and final this line only for github";
+
+		return author+"he is the best author of India .This is from the github now working in the same here";
+
 	}
 
 }
